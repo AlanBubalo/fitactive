024e4991430f14ba3a0dc693dd040d0fcb8f61e6
thomassiard <75135103+thomassiard@users.noreply.github.com>
2021-12-18 22:33:53 +0100

    Login Signup

---
+++ src/views/Signup.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="about">
+    <h1>Here you can signup!</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputName">Name</label>
+              <input
+                type="name"
+                class="form-control"
+                id="exampleInputName"
+                aria-describedby="NameHelp"
+                placeholder="First Name"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputSurname">Surname</label>
+              <input
+                type="surname"
+                class="form-control"
+                id="exampleInputSurname"
+                aria-describedby="SurnameHelp"
+                placeholder="Last Name"
+              />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+              >
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
