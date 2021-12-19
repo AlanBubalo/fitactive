bc1667d8a8d3e20686ef2ca3b02c515d18a6564a
thomassiard <75135103+thomassiard@users.noreply.github.com>
2021-12-19 18:30:56 +0100

    welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="welcome flex-container">
-    <img alt="FitActive logo" src="@/assets/fitactive.png" />
+    <div class="welcomeimage">
+    <img class="centered img" alt="FitActive logo" src="@/assets/welcome.jpg" />
+    <div class="centered">"All progress takes place outside the comfort zone."</div>
+    </div>
     <div>
       <router-link class="btn btn-primary login" role="button" to="/login">Login</router-link>
     </div>
-    <div>
-      <p>Getting started? <router-link class="signup" to=/signup>Signup</router-link></p>
+    <div class="signupbox">
+      <p>Getting started? <router-link class="signup" to=/signup>Sign up</router-link></p>
     </div>
   </div>
 </template>
@@ -13,12 +16,45 @@
 <style scoped lang="scss">
 .signup {
   text-decoration: none;
+  color:#F83E5F;
+  font-weight: bold;
 }
+
+.signupbox{
+  margin: 20px;
+}
+
 .login {
   padding: 12px 128px;
   margin-top: 32px;
   margin-bottom: 8px;
 }
+
+.btn-primary{
+  background-color: #F83E5F;
+  border-color: #F83E5F;
+  border-radius: 20px;
+}
+
+.img {
+  width: 100%;
+  height: 700px;
+  object-fit: cover;
+  object-position: 50% 23%;
+}
+
+// .welcomeimage {
+//   position: relative;
+//   text-align: center;
+//   color: white;
+// }
+
+// .centered{
+//   position: absolute;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+// }
 </style>
 
 <script>
