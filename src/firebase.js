4295ce36c4524f5cfba538526ab9cde1e2a7ce65
Alan Bubalo <alan.buba5@gmail.com>
2021-12-29 02:46:00 +0100

    Added Firebase

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDFuXG-3y14T5Q0J48wjXX7bCOUXCPhlVk",
+  authDomain: "fitactive-4f553.firebaseapp.com",
+  projectId: "fitactive-4f553",
+  storageBucket: "fitactive-4f553.appspot.com",
+  messagingSenderId: "552432539270",
+  appId: "1:552432539270:web:7a8a3eb4484f7dac35a307",
+  measurementId: "G-6Y054FJ5D0",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default {
+  firebase,
+};
